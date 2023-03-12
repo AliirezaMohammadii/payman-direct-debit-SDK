722c97c48aba8e1eab64e030138f8806b15e9ec2
Alireza Mohamadi <a.mohamadi@Faraboom.co>
2023-03-12 13:32:49 +0330

    add convert json to java object to http response mapper

--- src/main/java/com/ighe3/api/mapper/HttpResponseMapper.java
+++ src/main/java/com/ighe3/api/mapper/HttpResponseMapper.java
@@ -1,5 +1,16 @@
 package com.ighe3.api.mapper;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public final class HttpResponseMapper {
 
+    public static <T> Object convertJsonToJavaObject(String value, Class<T> responseClass) {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return mapper.readValue(value, responseClass);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
