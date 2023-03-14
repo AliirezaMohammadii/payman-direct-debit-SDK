b4ae4defda1fff4411609dbc966dc4af875c0f24
alireza mohammadi <ali9701mo@gmail.com>
2023-03-14 13:40:22 +0330

    tiny

--- src/main/java/com/ighe3/api/controller/CallbackController.java
+++ src/main/java/com/ighe3/api/controller/CallbackController.java
@@ -1,7 +1,9 @@
 package com.ighe3.api.controller;
 
 import com.ighe3.api.dto.enums.RedirectUrlStatus;
+import com.ighe3.api.exception.PaymanException;
 import com.ighe3.api.service.payman.PaymanIdService;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,15 +20,24 @@ public class CallbackController {
     }
 
     @GetMapping
-    public void createCallback(@RequestParam(name = "user_id") String userId,
+    public void handleCallback(@RequestParam(name = "user_id") String userId,
                                @RequestParam(name = "payman_code") String paymanCode,
-                               @RequestParam(name = "status") String creationStatus) throws RuntimeException {
+                               @RequestParam(name = "status") String status,
+                               // TODO: I'm not sure about "error" keyword
+                               @RequestParam(name = "error", required = false) String errorCode) throws RuntimeException {
+
+        // TODO: about errorMessage
+        if (errorCode != null) {
+            throw new PaymanException(errorCode, null, HttpStatus.INTERNAL_SERVER_ERROR.value());
+        }
 
         // TODO: study about factory design pattern
-        if (creationStatus.equals(RedirectUrlStatus.CREATED.name())) {
+        if (status.equals(RedirectUrlStatus.CREATED.name())) {
             String paymanId = paymanIdService.getPaymanId(paymanCode);
-        } else if (creationStatus.equals(RedirectUrlStatus.UPDATED.name())) {
+            // Store paymanId in database
+
+        } else if (status.equals(RedirectUrlStatus.UPDATED.name())) {
         } else
-            throw new RuntimeException("error");
+            throw new PaymanException(null, null, HttpStatus.INTERNAL_SERVER_ERROR.value());
     }
 }
