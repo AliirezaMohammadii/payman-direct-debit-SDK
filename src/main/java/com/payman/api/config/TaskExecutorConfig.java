0570806e035658bead312515842b15c78c4218fd
alireza mohammadi <ali9701mo@gmail.com>
2023-04-06 05:16:25 +0430

    tiny changes

--- src/main/java/com/payman/api/config/TaskExecutorConfig.java
+++ src/main/java/com/payman/api/config/TaskExecutorConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
-public class TaskExecutionConfig {
+public class TaskExecutorConfig {
 
     @Bean
     public ThreadPoolTaskExecutor threadPoolTaskExecutor() {
--- src/main/java/com/payman/api/service/impl/payman/CreateServiceImpl.java
+++ src/main/java/com/payman/api/service/impl/payman/CreateServiceImpl.java
@@ -3,6 +3,7 @@ package com.payman.api.service.impl.payman;
 import com.payman.api.config.UrlPropertiesConfig;
 import com.payman.api.dto.client.request.CreateRequest;
 import com.payman.api.dto.client.response.CreateResponse;
+import com.payman.api.idk.PaymanCreationTracer;
 import com.payman.api.mapper.RequestMapper;
 import com.payman.api.service.HttpService;
 import com.payman.api.dto.Response;
@@ -10,10 +11,12 @@ import com.payman.api.dto.provider.request.PaymanCreateRequest;
 import com.payman.api.service.payman.CreateService;
 import com.payman.api.utils.CustomHttpHeaders;
 import okhttp3.*;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.concurrent.Future;
 
 @Service
 public class CreateServiceImpl implements CreateService {
@@ -21,11 +24,20 @@ public class CreateServiceImpl implements CreateService {
     private final HttpService httpService;
     private final UrlPropertiesConfig urlPropertiesConfig;
     private final AccessTokenServiceImpl accessTokenService;
+    private final ThreadPoolTaskExecutor taskExecutor;
+    private final PaymanCreationTracer paymanCreationTracer;
 
-    public CreateServiceImpl(HttpService httpService, UrlPropertiesConfig urlPropertiesConfig, AccessTokenServiceImpl accessTokenService) {
+    public CreateServiceImpl(HttpService httpService,
+                             UrlPropertiesConfig urlPropertiesConfig,
+                             AccessTokenServiceImpl accessTokenService,
+                             ThreadPoolTaskExecutor taskExecutor,
+                             PaymanCreationTracer paymanCreationTracer) {
+
         this.httpService = httpService;
         this.urlPropertiesConfig = urlPropertiesConfig;
         this.accessTokenService = accessTokenService;
+        this.taskExecutor = taskExecutor;
+        this.paymanCreationTracer = paymanCreationTracer;
     }
 
     @Override
@@ -38,15 +50,21 @@ public class CreateServiceImpl implements CreateService {
 
         Response paymanResponse = httpService.sendRequest(paymanRequest, CreateService.class);
         Headers headers = paymanResponse.getHeaders();
+        traceCreationStatus(request);
         return new CreateResponse(headers.get("Location"));
+    }
+
+    private void traceCreationStatus(CreateRequest request) {
+        paymanCreationTracer.setUserId(request.getPayman().getUserId());
+        Future<?> future = taskExecutor.submit(paymanCreationTracer);
+        PaymanCreationTracer.ALL_TRACERS.put(request.getPayman().getUserId(), future);
     }
 
     private Headers createHeaders(HttpServletRequest httpServletRequest, CreateRequest request) {
         Headers generalHeaders = httpService.createHeaders(httpServletRequest, accessTokenService.getAccessToken());
-        Headers headers = generalHeaders.newBuilder()
+        return generalHeaders.newBuilder()
                 .add(CustomHttpHeaders.MOBILE_NO, request.getMobileNumber())
                 .add(CustomHttpHeaders.NATIONAL_CODE, request.getNationalCode())
                 .build();
-        return headers;
     }
 }
